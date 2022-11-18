ff92201400e5e01ceeea328f4d6743ef85ddcc34
MartaSyvyk <shavaliuk.marta@gmail.com>
2022-11-18 21:31:50 +0200

    simpleLightBox added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/styles.css';
 import axios from 'axios';
 import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 
 
@@ -8,20 +10,9 @@ const API_KEY = '31365930-c15782909a5a0e3024bcedf9d';
 const BASE_URL = 'https://pixabay.com/api/';
 const PER_PAGE = 40;
 let page = 1;
-let searchQuery = ''
-// function fetchImages (searchQuery, page) {
-//   return fetch(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&page=${page}&per_page=${PER_PAGE}&image_type=photo&orientation=horizontal&safesearch=true~`).then(response => {return response.json()});
-// }
+let searchQuery = '';
+let gallery = '';
 
-
-async function fetchImages(searchQuery, page) {
-  try {
-    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&page=${page}&per_page=${PER_PAGE}&image_type=photo&orientation=horizontal&safesearch=true~`);
-       return response.data;
-  } catch (error) {
-    console.error(error);
-  }
-}
 
 const refs = {
   searchForm: document.querySelector('.search-form'),
@@ -32,11 +23,24 @@ const refs = {
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreBtn.addEventListener('click', onLoadMore);
 
+async function fetchImages(searchQuery, page) {
+  try {
+    const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&page=${page}&per_page=${PER_PAGE}&image_type=photo&orientation=horizontal&safesearch=true~`);
+       return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
 function onSearch(event) {
   event.preventDefault();
   page = 1;
-      refs.gallery.innerHTML = "";
+  refs.gallery.innerHTML = "";
   searchQuery = event.currentTarget.elements.searchQuery.value;
+
+
   fetchImages (searchQuery, page).then((result) => {
      if (result.totalHits == 0) {
     Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
@@ -44,12 +48,13 @@ function onSearch(event) {
   }
     renderMarkup(result.hits);
     Notiflix.Notify.success(`Hooray! We found ${result.totalHits} images`);
-    
+    gallery = new SimpleLightbox('.gallery a');
     refs.loadMoreBtn.classList.remove ("visually-hidden");
   }
     ).catch(searchQuery => {console.log(searchQuery);
       Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")});
 }
+
 
 
 function onLoadMore (event) {
@@ -63,15 +68,16 @@ function onLoadMore (event) {
         return;
       }
     renderMarkup(result.hits);
-    
+    gallery.refresh();
     
   }).catch(error => console.log(error));
 }
 
 
 function renderMarkup (images) {
-  const newMarkup = images.map(image => { return  `<div class="photo-card">
-  <img class="image" src="${image.largeImageURL}" alt="" loading="lazy" />
+  const newMarkup = images.map(image => { return  ` <div class="photo-card">
+ <a href="${image.largeImageURL}">
+  <img class="image" src="${image.largeImageURL}" alt="" loading="lazy" /></a>
   <div class="info">
     <p class="info-item">
       <b>Likes</b></br>${image.likes}
@@ -86,7 +92,8 @@ function renderMarkup (images) {
       <b>Downloads</b></br>${image.downloads}
     </p>
   </div>
-</div>` }).join("");
+  
+</div` }).join("");
 refs.gallery.insertAdjacentHTML("beforeend", newMarkup);
 
 }
